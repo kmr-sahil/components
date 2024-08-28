4594ede5844822e5dbbbba02318358495fca0836
Sahil <sk2003311@gmail.com>
2024-08-28 20:23:55 +0530

    structure

---
+++ src/lib/componentRegistry.ts
@@ -0,0 +1,14 @@
+
+
+export const componentRegistry: Array<{
+  componentName: string;
+  componentPath: string;
+  codePath: string;
+}> = [
+  {
+    componentName: "Timer",
+    componentPath: "@/components/Timer",
+    codePath: "@/codeRegistry/TimerCode",
+  },
+  // Add more components here
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,22 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        backgroundOne: "rgba(var(--background-1))",
+        backgroundTwo: "rgba(var(--background-2))",
+        backgroundThree: "rgba(var(--background-3))",
+        accentOne: "rgba(var(--accent-1))",
+        accentTwo: "rgba(var(--accent-2))",
+        accentThree: "rgba(var(--accent-3))",
+        textOne: "rgba(var(--text-1))",
+        textTwo: "rgba(var(--text-2))",
+      },
+      borderWidth: {
+        special: "rgba(var(--stroke))",
+      }
     },
   },
+  darkMode: "class",
   plugins: [],
 };
 export default config;
